523e9bc267d7a7ed492c6b4a729979039f4381aa
Mark Norman Francis <norm@cackhanded.net>
2016-04-10 11:41:30 +0100

    Add homepage
    The base template informs us if the user is logged in. At this stage,
    that can only be done via the admin interface.

--- customuser/urls.py
+++ customuser/urls.py
@@ -15,10 +15,20 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.views.generic import TemplateView
+
 
 urlpatterns = [
     url(
         r'^admin/',
         admin.site.urls,
+    ),
+
+    url(
+        r'^$',
+        TemplateView.as_view(
+            template_name='homepage.html',
+        ),
+        name='homepage',
     ),
 ]
